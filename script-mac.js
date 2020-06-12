317e52d8461532c8f66e62d8e7d1c664d99b882b
Neil Soiffer <soiffer@alum.mit.edu>
2020-06-12 01:06:30 -0700

    Lots of changes and cleanup Copy to/from edit/web page works Buttons work Speech works, at least as much as it can in your browser. TextHelp button work ssml and jasonized version are always in sync with edit area.
    What remains:
    still lots of trouble fetching web pages
    selection from web page area isn't right
    CSS styling needs work

--- script-mac.js
+++ script-mac.js
@@ -1,246 +1,241 @@
+// @ts-check
+/* -*- Mode: Java; tab-width: 4; indent-tabs-mode:nil; c-basic-offset: 4 -*- */
+/* vim: set ts=4 et sw=4 tw=80: */
+
 /* SSML Test Tool - Supporting Javascript
 	Author Mark Hakkinen (mhakkinen@ets.org) and Irfan Ali (iali@ets.org)
 	Last modifed May 21, 2018
 */
-var synth = window.speechSynthesis;
 
-var inputTxt = document.querySelector('#AuthoredText');
-var voiceSelect = document.querySelector('select');
+var synth = null;
 
-// Adding Stop button to stop the synthesiser 
-var stopButton = document.getElementById('stopButton');
-stopButton.addEventListener('click', function(e) {
-			synth.cancel();
-		
-		});
+var voiceSelect = null;
+
+window.addEventListener("load", function () {
+	// Adding Stop button to stop the synthesiser 
+	var stopButton = document.getElementById('stopButton');
+	stopButton.addEventListener('click', function (e) {
+		synth.cancel();
+	});
+
+	// adding xml wrapper for windows synthesiser
+	if (navigator.appVersion.indexOf("Win") != -1) {
+		document.getElementById('chkXMLprefix').checked = true;
+		document.getElementById('chkWrapSSML').checked = true;
+	}
+
+	synth = window.speechSynthesis;
+	voiceSelect = document.querySelector('select');
+	populateVoiceList();
+	if (speechSynthesis.onvoiceschanged !== undefined) {
+		speechSynthesis.onvoiceschanged = populateVoiceList;
+	}
+
+});
+
 // stop the synthesiser  ends
-var voices = [];
+let voices = [];
 
 function populateVoiceList() {
-  voices = synth.getVoices();
+	voices = synth.getVoices();
 
-  for(i = 0; i < voices.length ; i++) {
-    var option = document.createElement('option');
-    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
-    
-    if(voices[i].default) {
-      option.textContent += ' -- DEFAULT';
-    }
+	for (i = 0; i < voices.length; i++) {
+		var option = document.createElement('option');
+		option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
 
-    option.setAttribute('data-lang', voices[i].lang);
-    option.setAttribute('data-name', voices[i].name);
-    voiceSelect.appendChild(option);
-  }
-}
-
-populateVoiceList();
-if (speechSynthesis.onvoiceschanged !== undefined) {
-  speechSynthesis.onvoiceschanged = populateVoiceList;
-}
-
-function displayXML(s)
-{
-  var wst;
-  wst=s;
-  while (wst.indexOf("<")>=0) wst=wst.replace("<","&lt;");
-  while (wst.indexOf(">")>=0) wst=wst.replace(">","&gt;");
-  return wst;
-}
-
-// adding xml wrapper for windows synthesiser
-if (navigator.appVersion.indexOf("Win")!=-1) {
-	document.getElementById('chkXMLprefix').checked=true;
-  document.getElementById('chkWrapSSML').checked=true;
-}
-	
-
-function produceSpeech(){
-	
-  var workstring=inputTxt.value;
-  var prefix;
-  var wrapper;
-	var json2ssml = document.getElementById('chkJSONAttr').checked;
-	var bWrap=document.getElementById('chkWrapSSML').checked;
-    var bPrefix=document.getElementById('chkXMLprefix').checked; 
-    var bDataAttr=document.getElementById('chkDataAttr').checked;
- 
- 
-  var msSpeakTag='<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" ' + 
-  'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" '+
-  'xsi:schemaLocation="http://www.w3.org/2001/10/synthesis '+
-  'http://www.w3.org/TR/speech-synthesis/synthesis.xsd" xml:lang="en-US">';
-
-	if (bWrap) {workstring=msSpeakTag+workstring+"</speak>";};
-
-// Function to convert json HTML to SSML
-
- if (json2ssml) {
-
-// ssml : ssmlval : [prevTxt, postTxt]
-
-		var two = {
-"prosody": {
-	 "fast":["[[rate 300]]", "[[rset 0]]"], 
-   "slow":["[[rate 75]]", "[[rset 0]]"], 
-	 "x-fast":["[[rate 300]]", "[[rset 0]]"]
-	},
-"say-as":
-	{
-		"characters":["[[char LTRL]]", "[[char NORM]]"],
-		"ordinal":["[[nmbr NORM]]", "[[nmbr NORM]]"],
-		"cardinal" : ["[[nmbr NORM]]", "[[nmbr NORM]]"],
-		"date" :["[[Day][/][Month][/][Year]]", "[[Day][/][Month][/][Year]]"],
-		"time" :["[[hour][:][minute][:][second]]", "[[hour][:][minute][:][second]]"]
-		},
-"emphasis" :{
-		"strong" :["[[emph +]]", "[[rset 0]]"],
-		"moderate" : ["[[emph +; emph +]]", "[[rset 0]]"],
-		"reduced" :["[[emph -]]", "[[rset 0]]"]
-	},
-	"break": {
-        "weak"  : ["[[slnc 250]]", "[[slnc 0]]"],
-		 "medium" : ["[[slnc 500]]", "[[slnc 0]]"],         
-		 "strong" : ["[[slnc 1000]]", "[[slnc 0]]"],
-		 "time" : ["[[second]]", "[[second]]"]
-
-		},
-	
-   "sub" : {
-	 "alias" : ["[[]]"],
-	 "red" : ["[[]]", "[[]]"],
-	 "World Wide Web Consortium" : ["[[]]", "[[]]"],
-	 "Speech Synthesis Markup Language" : ["[[]]", "[[]]"]
-			
-			
+		if (voices[i].default) {
+			option.textContent += ' -- DEFAULT';
 		}
 
+		option.setAttribute('data-lang', voices[i].lang);
+		option.setAttribute('data-name', voices[i].name);
+		voiceSelect.appendChild(option);
+	}
 }
 
 
- var ssml2Parse;
- ssml2Parse = workstring;
- var elssml = document.getElementById('pstruct');
- elssml.innerHTML=ssml2Parse;
- var elems = elssml.getElementsByTagName('span');
-  
-// checking for window operative system only
-
-if (navigator.appVersion.indexOf("Win")>=1) {
-					var safe = elems.length;
-					while (elems[0] && safe) {
-						safe--;
-						var elem = elems[0];
-						var ssml = JSON.parse(elem.dataset.ssml);
-						
-						var innerMost = null; // to clear the previous value from the DOM
-						var rssml = []; // creating the array of ssml
-						for (var t in ssml) {
-							
-							rssml.push(t);
-						}
-						//['parameter1','parameter2','parameter3']
-						// staring looping in the reverse order from last child 
-						for (var i = rssml.length - 1; i >= 0; i--) {
-							var tagName = rssml[i];
-
-							var child = document.createElement(tagName); 
-							// ssml[tagName] :: {"property":"attribute"}
-							for (var a in ssml[tagName]) {
-								child.setAttribute(a, ssml[tagName][a]);
-							}
-							if (innerMost == null) {
-								child.innerText = elem.innerText;
-							} else {
-								child.appendChild(innerMost);
-							}
-							innerMost = child;
-						}
-						elem.parentNode.replaceChild(child, elem);
-						
-						
-					} 
-				
+function displayXML(s) {
+	var wst;
+	wst = s;
+	while (wst.indexOf("<") >= 0) wst = wst.replace("<", "&lt;");
+	while (wst.indexOf(">") >= 0) wst = wst.replace(">", "&gt;");
+	return wst;
 }
-// window operative system
 
-else {
-	    // var elems = document.getElementsByTagName("span");
-				var safe = elems.length;
-				while (elems[0] && safe) {
-					safe--;
-					var s = elems[0];
-					//console.log("source:"+s);
-					var ssml = JSON.parse(s.dataset.ssml);
-					for (var i in ssml) {
-						for (var j in two) {
-						//	console.log("i: "+i + ", j: "+j);
-							if (i == j) {
-								for (var k in ssml[i]) {
-									for (var l in two[j]) {
+function jasonToSSML(workString) {
+	// ssml : ssmlval : [prevTxt, postTxt]
+
+	var two = {
+		"prosody": {
+			"fast": ["[[rate 300]]", "[[rset 0]]"],
+			"slow": ["[[rate 75]]", "[[rset 0]]"],
+			"x-fast": ["[[rate 300]]", "[[rset 0]]"]
+		},
+		"say-as":
+		{
+			"characters": ["[[char LTRL]]", "[[char NORM]]"],
+			"ordinal": ["[[nmbr NORM]]", "[[nmbr NORM]]"],
+			"cardinal": ["[[nmbr NORM]]", "[[nmbr NORM]]"],
+			"date": ["[[Day][/][Month][/][Year]]", "[[Day][/][Month][/][Year]]"],
+			"time": ["[[hour][:][minute][:][second]]", "[[hour][:][minute][:][second]]"]
+		},
+		"emphasis": {
+			"strong": ["[[emph +]]", "[[rset 0]]"],
+			"moderate": ["[[emph +; emph +]]", "[[rset 0]]"],
+			"reduced": ["[[emph -]]", "[[rset 0]]"]
+		},
+		"break": {
+			"weak": ["[[slnc 250]]", "[[slnc 0]]"],
+			"medium": ["[[slnc 500]]", "[[slnc 0]]"],
+			"strong": ["[[slnc 1000]]", "[[slnc 0]]"],
+			"time": ["[[second]]", "[[second]]"]
+
+		},
+
+		"sub": {
+			"alias": ["[[]]"],
+			"red": ["[[]]", "[[]]"],
+			"World Wide Web Consortium": ["[[]]", "[[]]"],
+			"Speech Synthesis Markup Language": ["[[]]", "[[]]"]
+
+
+		}
+
+	}
+
+
+	var ssml2Parse;
+	ssml2Parse = workString;
+	var ssmlEl = document.getElementById('ssml-string');
+	var elems = ssmlEl.getElementsByTagName('span');
+
+	// checking for window operative system only
+
+	if (navigator.appVersion.indexOf("Win") >= 1) {
+		var safe = elems.length;
+		while (elems[0] && safe) {
+			safe--;
+			var elem = elems[0];
+			var ssml = JSON.parse(elem.dataset.ssml);
+
+			var innerMost = null; // to clear the previous value from the DOM
+			var rssml = []; // creating the array of ssml
+			for (var t in ssml) {
+
+				rssml.push(t);
+			}
+			//['parameter1','parameter2','parameter3']
+			// staring looping in the reverse order from last child 
+			for (var i = rssml.length - 1; i >= 0; i--) {
+				var tagName = rssml[i];
+
+				var child = document.createElement(tagName);
+				// ssml[tagName] :: {"property":"attribute"}
+				for (var a in ssml[tagName]) {
+					child.setAttribute(a, ssml[tagName][a]);
+				}
+				if (innerMost == null) {
+					child.innerText = elem.innerText;
+				} else {
+					child.appendChild(innerMost);
+				}
+				innerMost = child;
+			}
+			elem.parentNode.replaceChild(child, elem);
+
+
+		}
+
+	}
+	// window operative system
+
+	else {
+		// var elems = document.getElementsByTagName("span");
+		var safe = elems.length;
+		while (elems[0] && safe) {
+			safe--;
+			var s = elems[0];
+			//console.log("source:"+s);
+			var ssml = JSON.parse(s.dataset.ssml);
+			for (var i in ssml) {
+				for (var j in two) {
+					//	console.log("i: "+i + ", j: "+j);
+					if (i == j) {
+						for (var k in ssml[i]) {
+							for (var l in two[j]) {
 								//		console.log("ssml[i][k]: "+ssml[i][k] + ", l: "+l);
-										if (ssml[i][k] == l) {
-										//	console.log("match");
+								if (ssml[i][k] == l) {
+									//	console.log("match");
 									//	console.log(s.out);
-											s.outerHTML =  two[j][l][0] + s.innerHTML + two[j][l][1];
-								    
-										}
-										
-									}
+									s.outerHTML = two[j][l][0] + s.innerHTML + two[j][l][1];
+
 								}
+
 							}
 						}
 					}
 				}
+			}
+		}
 
+	}
+};
+
+
+function produceSpeech() {
+	let workString = document.getElementById('ssml-string').innerText;
+	var prefix;
+	var wrapper;
+	var json2ssml = document.getElementById('chkJSONAttr').checked;
+	var bWrap = document.getElementById('chkWrapSSML').checked;
+	var bPrefix = document.getElementById('chkXMLprefix').checked;
+	var bDataAttr = document.getElementById('chkDataAttr').checked;
+
+
+	var msSpeakTag = '<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" ' +
+		'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
+		'xsi:schemaLocation="http://www.w3.org/2001/10/synthesis ' +
+		'http://www.w3.org/TR/speech-synthesis/synthesis.xsd" xml:lang="en-US">';
+
+	if (bWrap) {
+		workString = msSpeakTag + workString + "</speak>";
+	};
+
+	// Function to convert json HTML to SSML
+
+	jasonToSSML(workString)
+
+
+	if (bPrefix) {
+		workString = "<?xml version='1.0'?>" + workString;
+	};
+
+	var utterThis = new SpeechSynthesisUtterance(workString);
+
+	var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+	for (let i = 0; i < voices.length; i++) {
+		if (voices[i].name === selectedOption) {
+			utterThis.voice = voices[i];
+			utterThis.rate = 1;
+		}
+	}
+
+	synth.speak(utterThis);
 }
-//if not chrome
 
-
- };
-
-	if (bPrefix) {workstring="<?xml version='1.0'?>"+workstring;};
-//  if (bPrefix) {workstring=workstring;};
- 
-
-var utterThis = new SpeechSynthesisUtterance(workstring);
-
-if (json2ssml) {
-	var utterThis = new SpeechSynthesisUtterance(document.getElementById('pstruct').innerHTML);
-}
-
-  var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
-  for(i = 0; i < voices.length ; i++) {
-    if(voices[i].name === selectedOption) {
-      utterThis.voice = voices[i];
-	
-	  utterThis.rate=1;
-	  
-    }
-  }
-
-  synth.speak(utterThis);
-  document.getElementById('result').innerHTML=displayXML(workstring);
-  document.getElementById('pstruct').innerHTML=displayXML(document.getElementById('pstruct').innerHTML);
-
-  
-}
- 
-function getBrowserInfo()
-{
+function getBrowserInfo() {
 	var ua = navigator.userAgent, tem,
-	M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-	if(/trident/i.test(M[1]))
-	{
-		tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
-		return 'IE '+(tem[1] || '');
+		M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+	if (/trident/i.test(M[1])) {
+		tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+		return 'IE ' + (tem[1] || '');
 	}
-	if(M[1]=== 'Chrome')
-	{
-		tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
-		if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+	if (M[1] === 'Chrome') {
+		tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
+		if (tem != null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
 	}
-	M = M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-	if((tem= ua.match(/version\/(\d+)/i))!= null) 
+	M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+	if ((tem = ua.match(/version\/(\d+)/i)) != null)
 		M.splice(1, 1, tem[1]);
 	return M.join(' ');
 }
--- script.js
+++ script.js
@@ -1,17 +1,27 @@
+// @ts-check
+/* -*- Mode: Java; tab-width: 4; indent-tabs-mode:nil; c-basic-offset: 4 -*- */
+/* vim: set ts=4 et sw=4 tw=80: */
+
 window.addEventListener("load", function () {
 
-    function setSSMLAttr(attrValue) {
+    function setSSMLAttr(attrValue, requireSelection) {
+        requireSelection = (requireSelection !== null) || true;
         const textArea = document.getElementById("AuthoredText");
         const doc = textArea.ownerDocument || textArea.document;
         const win = doc.defaultView || doc.parentWindow;
         if (typeof win.getSelection != "undefined") {
             const sel = win.getSelection();
-            if (sel && sel.rangeCount > 0 && !sel.isCollapsed) {
-                const spanWrapper = document.createElement('span');
-                spanWrapper.setAttribute('data-ssml', attrValue);
-
-                const range = sel.getRangeAt(0);     // only handle single selection
-                range.surroundContents(spanWrapper);
+            if (sel && sel.rangeCount > 0) {
+                if ( !(requireSelection && sel.isCollapsed)) {
+                    const spanWrapper = document.createElement('span');
+                    spanWrapper.setAttribute('data-ssml', attrValue);
+    
+                    const range = sel.getRangeAt(0);     // only handle single selection
+                    if (!requireSelection) {
+                        range.collapse(true);   // assure cursor at start
+                    }
+                    range.surroundContents(spanWrapper);    
+                }
             }
         }
     }
@@ -22,7 +32,7 @@ window.addEventListener("load", function () {
     document.getElementById("sayCardinal").onclick = (() => setSSMLAttr('{"say-as": {"interpret-as": "cardinal"}}'));
     document.getElementById("sayOrdinal").onclick = (() => setSSMLAttr('{"say-as": {"interpret-as": "ordinal"}}'));
     document.getElementById("sayChar").onclick = (() => setSSMLAttr('{"say-as": {"interpret-as": "characters"}}'));
-    document.getElementById("breakMedium").onclick = (() => setSSMLAttr('{"break": {"strength":"medium"}}'));
+    document.getElementById("breakMedium").onclick = (() => setSSMLAttr('{"break": {"strength":"medium"}}', false));
     document.getElementById("subAlias").onclick = function() {
         const substitutionText = prompt("Enter substitution text:")
         if (substitutionText) {
@@ -30,23 +40,34 @@ window.addEventListener("load", function () {
         }
     };
 
-
+    new MutationObserver(updateSSMLAreas).observe(
+        this.document.getElementById("AuthoredText"),
+        {childList: true, characterData: true, subtree: true});
 });
 
 // From: https://stackoverflow.com/questions/247483/http-get-request-in-javascript/4033310#4033310
 var HttpClient = function () {
     this.get = function (aUrl, aCallback) {
-        var anHttpRequest = new XMLHttpRequest();
-        anHttpRequest.responseType = "document";
-        anHttpRequest.onload = function () {
-            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-                aCallback(anHttpRequest.responseXML);
+        try {
+            const anHttpRequest = new XMLHttpRequest();
+            anHttpRequest.responseType = "document";
+            anHttpRequest.onload = function () {
+                if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                    aCallback(anHttpRequest.responseXML);
+            }
+    
+            anHttpRequest.open("GET", aUrl, true);
+            anHttpRequest.send(null);
+        } catch(error) {
+            alert("Failed to get data from url. Reason: " + JSON.stringify(error));
         }
-
-        anHttpRequest.open("GET", aUrl, true);
-        anHttpRequest.send(null);
     }
 }
+
+/**
+ * 
+ * @param {string} url 
+ */
 function getFromURL(url) {
     url = url || prompt("Enter URL of page to edit");
     if (url !== null) {
@@ -61,3 +82,112 @@ function getFromURL(url) {
     }
 }
 
+/**
+ * @returns {Element}
+ */
+function getWebContentSelection() {
+    const textArea = document.getElementById("webContent");
+    const doc = textArea.ownerDocument || textArea.document;
+    const win = doc.defaultView || doc.parentWindow;
+    if (typeof win.getSelection != "undefined") {
+        const sel = win.getSelection();
+        if (sel && sel.rangeCount > 0 && !sel.isCollapsed) {
+            return sel.getRangeAt(0).commonAncestorContainer;     // only handle single selection
+        }
+    }
+}
+
+
+/**
+ * 
+ * @param {Element} el 
+ * @returns {string}
+ */
+function nodeToSSML(el) {
+	let result = '';
+	if (el.tagName === 'SPAN' && el.hasAttribute('data-ssml')) {
+        // open tag and attrs
+		const keyValArray = Object.entries(JSON.parse(el.getAttribute('data-ssml')));
+		keyValArray.forEach(([tag, attrObject]) => {
+			result += `<${tag}`;
+			Object.entries(attrObject).forEach(([attrName, attrValue]) => {
+				result += ` ${attrName}="${attrValue}"`;
+			})
+		});
+		result += '>'
+    }
+
+	const children = el.childNodes;
+    if (children.length === 0) {     // <span>s might have text but no children
+		return el.textContent;
+	} 
+
+	for (let i = 0; i < children.length; i++) {
+		result += nodeToSSML(children[i]);
+	}
+
+	if (el.tagName === 'SPAN' && el.hasAttribute('data-ssml')) {
+		// matching close tag (go from last to first)
+		const keyValArray = Object.entries(JSON.parse(el.getAttribute('data-ssml'))).reverse();
+		keyValArray.forEach(([tag, attrObject]) => {
+			result += `</${tag}>`;
+		});
+    }
+    return result;
+}
+
+    /**
+     * @param {[MutationRecord]} mutationList 
+     * @param {MutationObserver} observer 
+     */
+    function updateSSMLAreas(mutationList, observer) {
+        // root node id='AuthoredText'
+        const rootElement = document.getElementById("AuthoredText");
+        //observer.disconnect();
+        const ssmlString = nodeToSSML(rootElement);
+        document.getElementById('ssml-string').textContent = ssmlString;
+        document.getElementById('json-string').textContent = rootElement.innerHTML.replace(/\&quot;/g, "'");
+        //observer.observe(mutation.target);
+    }
+
+
+
+function removeOldID() {
+    const el = document.querySelector('[id="XXX-web-selection-XXX"]');
+    if (el) {
+        el.removeAttribute('id');
+        if (el.hasAttribute('data-savedID')) {
+            el.setAttribute('id', el.getAttribute('data-savedID'));
+            el.removeAttribute('data-savedID');
+        }    
+    }
+}
+
+function pasteFromWeb() {
+    const selectedElement = getWebContentSelection();
+    if (!selectedElement) {
+        alert('Unable to copy selection');
+        return;
+    }
+
+    removeOldID();
+    if (selectedElement.id) {
+        selectedElement.setAttribute('data-savedID', selectedElement.id);
+    }
+    
+    const authorElement = document.getElementById("AuthoredText");
+    authorElement.textContent = '';
+    authorElement.appendChild(selectedElement.cloneNode(true));
+    selectedElement.id = 'XXX-web-selection-XXX';       // do this after cloning so not part of clone
+
+}
+
+function pasteToWeb() {
+    // not yet implemented
+    const target = document.getElementById('XXX-web-selection-XXX');
+    if (target) {
+        const authorElement = document.getElementById("AuthoredText");
+        target.innerHTML = authorElement.innerHTML;
+        // don't remove the old target id -- may want to paste again.
+    }
+}
