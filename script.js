23468ec29912db6d2463e6a15652d51952498034
Neil Soiffer <soiffer@alum.mit.edu>
2020-06-12 10:43:02 -0700

    Added filereader to replace http get

--- script.js
+++ script.js
@@ -12,15 +12,15 @@ window.addEventListener("load", function () {
         if (typeof win.getSelection != "undefined") {
             const sel = win.getSelection();
             if (sel && sel.rangeCount > 0) {
-                if ( !(requireSelection && sel.isCollapsed)) {
+                if (!(requireSelection && sel.isCollapsed)) {
                     const spanWrapper = document.createElement('span');
                     spanWrapper.setAttribute('data-ssml', attrValue);
-    
+
                     const range = sel.getRangeAt(0);     // only handle single selection
                     if (!requireSelection) {
                         range.collapse(true);   // assure cursor at start
                     }
-                    range.surroundContents(spanWrapper);    
+                    range.surroundContents(spanWrapper);
                 }
             }
         }
@@ -33,17 +33,20 @@ window.addEventListener("load", function () {
     document.getElementById("sayOrdinal").onclick = (() => setSSMLAttr('{"say-as": {"interpret-as": "ordinal"}}'));
     document.getElementById("sayChar").onclick = (() => setSSMLAttr('{"say-as": {"interpret-as": "characters"}}'));
     document.getElementById("breakMedium").onclick = (() => setSSMLAttr('{"break": {"strength":"medium"}}', false));
-    document.getElementById("subAlias").onclick = function() {
+    document.getElementById("subAlias").onclick = function () {
         const substitutionText = prompt("Enter substitution text:")
         if (substitutionText) {
             setSSMLAttr(`{"sub": {"alias":"${substitutionText}"}}`);
         }
     };
 
+
+    updateSSMLAreas(null, null);
     new MutationObserver(updateSSMLAreas).observe(
         this.document.getElementById("AuthoredText"),
-        {childList: true, characterData: true, subtree: true});
+        { childList: true, characterData: true, subtree: true });
 });
+
 
 // From: https://stackoverflow.com/questions/247483/http-get-request-in-javascript/4033310#4033310
 var HttpClient = function () {
@@ -55,10 +58,10 @@ var HttpClient = function () {
                 if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
                     aCallback(anHttpRequest.responseXML);
             }
-    
+
             anHttpRequest.open("GET", aUrl, true);
             anHttpRequest.send(null);
-        } catch(error) {
+        } catch (error) {
             alert("Failed to get data from url. Reason: " + JSON.stringify(error));
         }
     }
@@ -66,20 +69,27 @@ var HttpClient = function () {
 
 /**
  * 
- * @param {string} url 
+ * @param {[File]} files 
  */
-function getFromURL(url) {
-    url = url || prompt("Enter URL of page to edit");
-    if (url !== null) {
-        url = 'https://yacdn.org/proxy/' + (url.includes('://') ? '' : 'https://') + url;
-        var client = new HttpClient();
-        client.get(url, function (response) {
-            // do something with response
-            const el = document.getElementById('webContent');
-            el.appendChild(response.body.cloneNode(true));
-            $rw_tagSentences();
-        });
+function getFromURL(files) {
+    if (files.length === 0) {
+        return;
     }
+    const url = files[0]
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        const contents = e.target.result.match(/<body[\s\S]*<\/body>/i);
+        if (contents.length !== 1) {
+            alert("Retrieved file did not have a body");
+        }
+        const el = document.getElementById('webContent');
+        el.innerHTML = contents[0];
+        $rw_tagSentences();
+        reader.onerror = function (e) {
+            alert("Unable to read file: " + JSON.stringify(e));
+        };
+    }
+    reader.readAsText(url);
 }
 
 /**
@@ -104,51 +114,51 @@ function getWebContentSelection() {
  * @returns {string}
  */
 function nodeToSSML(el) {
-	let result = '';
-	if (el.tagName === 'SPAN' && el.hasAttribute('data-ssml')) {
+    let result = '';
+    if (el.tagName === 'SPAN' && el.hasAttribute('data-ssml')) {
         // open tag and attrs
-		const keyValArray = Object.entries(JSON.parse(el.getAttribute('data-ssml')));
-		keyValArray.forEach(([tag, attrObject]) => {
-			result += `<${tag}`;
-			Object.entries(attrObject).forEach(([attrName, attrValue]) => {
-				result += ` ${attrName}="${attrValue}"`;
-			})
-		});
-		result += '>'
+        const keyValArray = Object.entries(JSON.parse(el.getAttribute('data-ssml')));
+        keyValArray.forEach(([tag, attrObject]) => {
+            result += `<${tag}`;
+            Object.entries(attrObject).forEach(([attrName, attrValue]) => {
+                result += ` ${attrName}="${attrValue}"`;
+            })
+        });
+        result += '>'
     }
 
-	const children = el.childNodes;
+    const children = el.childNodes;
     if (children.length === 0) {     // <span>s might have text but no children
-		return el.textContent;
-	} 
+        return el.textContent;
+    }
 
-	for (let i = 0; i < children.length; i++) {
-		result += nodeToSSML(children[i]);
-	}
+    for (let i = 0; i < children.length; i++) {
+        result += nodeToSSML(children[i]);
+    }
 
-	if (el.tagName === 'SPAN' && el.hasAttribute('data-ssml')) {
-		// matching close tag (go from last to first)
-		const keyValArray = Object.entries(JSON.parse(el.getAttribute('data-ssml'))).reverse();
-		keyValArray.forEach(([tag, attrObject]) => {
-			result += `</${tag}>`;
-		});
+    if (el.tagName === 'SPAN' && el.hasAttribute('data-ssml')) {
+        // matching close tag (go from last to first)
+        const keyValArray = Object.entries(JSON.parse(el.getAttribute('data-ssml'))).reverse();
+        keyValArray.forEach(([tag, attrObject]) => {
+            result += `</${tag}>`;
+        });
     }
     return result;
 }
 
-    /**
-     * @param {[MutationRecord]} mutationList 
-     * @param {MutationObserver} observer 
-     */
-    function updateSSMLAreas(mutationList, observer) {
-        // root node id='AuthoredText'
-        const rootElement = document.getElementById("AuthoredText");
-        //observer.disconnect();
-        const ssmlString = nodeToSSML(rootElement);
-        document.getElementById('ssml-string').textContent = ssmlString;
-        document.getElementById('json-string').textContent = rootElement.innerHTML.replace(/\&quot;/g, "'");
-        //observer.observe(mutation.target);
-    }
+/**
+ * @param {[MutationRecord]} mutationList 
+ * @param {MutationObserver} observer 
+ */
+function updateSSMLAreas(mutationList, observer) {
+    // root node id='AuthoredText'
+    const rootElement = document.getElementById("AuthoredText");
+    //observer.disconnect();
+    const ssmlString = nodeToSSML(rootElement);
+    document.getElementById('ssml-string').textContent = ssmlString;
+    document.getElementById('json-string').textContent = rootElement.innerHTML.replace(/\&quot;/g, "'");
+    //observer.observe(mutation.target);
+}
 
 
 
@@ -159,7 +169,7 @@ function removeOldID() {
         if (el.hasAttribute('data-savedID')) {
             el.setAttribute('id', el.getAttribute('data-savedID'));
             el.removeAttribute('data-savedID');
-        }    
+        }
     }
 }
 
@@ -174,7 +184,7 @@ function pasteFromWeb() {
     if (selectedElement.id) {
         selectedElement.setAttribute('data-savedID', selectedElement.id);
     }
-    
+
     const authorElement = document.getElementById("AuthoredText");
     authorElement.textContent = '';
     authorElement.appendChild(selectedElement.cloneNode(true));
